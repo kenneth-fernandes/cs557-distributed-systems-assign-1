e535351d9f4cfdb2496c2e23ce616f244619fe58
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2020-09-09 21:28:27 -0400

    Kenneth: Added structure for client and server

---
+++ httpServerMultiThread/src/httpServerMultiThread/client/driver/Driver.java
@@ -0,0 +1,7 @@
+package client.driver;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.print("Hello Client");
+    }
+}
---
+++ httpServerMultiThread/src/httpServerMultiThread/server/driver/Driver.java
@@ -0,0 +1,7 @@
+package server.driver;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.print("Hello Server");
+    }
+}
