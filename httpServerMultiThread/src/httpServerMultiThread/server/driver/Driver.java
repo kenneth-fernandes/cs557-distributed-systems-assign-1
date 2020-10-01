2d02891638dee8595ce2a42ed95d0c7e36a546db
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2020-10-01 16:32:54 -0400

    Deleted code for adding port number from command line.

--- httpServerMultiThread/src/httpServerMultiThread/server/driver/Driver.java
+++ httpServerMultiThread/src/httpServerMultiThread/server/driver/Driver.java
@@ -6,12 +6,12 @@ import server.socket.HttpServer;
 
 public class Driver {
     public static void main(String[] args) {
-        if (args.length != 1 || args[0].equals("${portNumber}")) {
+        /*if (args.length != 1 || args[0].equals("${portNumber}")) {
             System.err.println("Error: Incorrect number of arguments. Program accepts 1 argumnets.");
             System.exit(0);
-        }
+        }*/
         try {
-            HttpServer server = new HttpServer(Integer.parseInt(args[0]));
+            HttpServer server = new HttpServer(8080);
             server.processRequest();
         } catch (NumberFormatException | IOException e) {
             e.printStackTrace();
